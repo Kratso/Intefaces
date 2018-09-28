91115799c3ddbd04ff67364ecbdf4e04d64fd997
Kratso <kratsothelsamar@gmail.com>
2018-09-28 14:24:58 +0200

    Terminal No funciona Matame

--- BuscadorTerminalImpostor/src/buscadorterminalimpostor/TerminalImpostorFXMLController.java
+++ BuscadorTerminalImpostor/src/buscadorterminalimpostor/TerminalImpostorFXMLController.java
@@ -5,9 +5,23 @@
  */
 package buscadorterminalimpostor;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 /**
  * FXML Controller class
@@ -15,13 +29,61 @@ import javafx.fxml.Initializable;
  * @author krats
  */
 public class TerminalImpostorFXMLController implements Initializable {
+    
+    @FXML
+    private TextField _command_prompt;
+
+    @FXML
+    private TextArea _text_area;
+
+    private static Set<String> commandSet = new TreeSet();
+    private static int currentCommand;
+
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+    }
+
+    @FXML
+    private void executeLine(KeyEvent event) throws IOException{
+        if (event.getCode().isArrowKey()) {
+            if (event.getCode().equals(KeyCode.UP)) {
+                if(currentCommand == 0){
+                    currentCommand = commandSet.size();
+                }
+                if (currentCommand > 0) {
+                    _command_prompt.setText((String) commandSet.toArray()[currentCommand - 1]);
+                }
+            } else if (event.getCode().equals(KeyCode.DOWN)) {
+                currentCommand++;
+                if (currentCommand < commandSet.size()) {
+                    _command_prompt.setText((String) commandSet.toArray()[currentCommand - 1]);
+                } else {
+                    _command_prompt.setText("");
+                    currentCommand = 0;
+                }
+            }
+        } else {
+            if (event.getCode().equals(KeyCode.ENTER)) {
+                String command = _command_prompt.getText();
+                commandSet.add(command);
+                parseCommand(command);
+                currentCommand = 0;
+                _command_prompt.setText("");
+                String area = "";
+                for(String s : commandSet){
+                    area += s + "\n";
+                }
+                _text_area.setText(area);
+            }
+        }
+    }
+
+    private void parseCommand(String command) {
+        System.out.println("klsjdndlsfnm");
+    }
+
 }
