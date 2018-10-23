7301d5105b98581d2f043e0fcf641f697d0e3d03
Kratso <kratsothelsamar@gmail.com>
2018-10-23 12:52:55 +0200

    juas juas

--- GestorVentanas1FX/src/gestorventanas1fx/FXMLGestorVentas1Controller.java
+++ GestorVentanas1FX/src/gestorventanas1fx/FXMLGestorVentas1Controller.java
@@ -6,7 +6,11 @@
 package gestorventanas1fx;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.Vector;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -18,128 +22,198 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 /**
  *
  * @author alumno
  */
 public class FXMLGestorVentas1Controller implements Initializable {
-	
-	@FXML
-	private TextField nombreField;
-	
-	@FXML
-	private Button botonAniadir;
-	
-	@FXML
-    private Button botonAnterior;
-	
-	@FXML
-    private Button botonBuscar;
-	
-	@FXML
-    private Button botonCancelar;
-	
-	@FXML
-    private Button botonEliminar;
-	
-	@FXML
-    private Button botonPrimero;
-	
-	@FXML
-    private Button botonSalir;
-	
-	@FXML
-    private Button botonSiguiente;
-	
-	@FXML
-    private Button botonUltimo;
-	
-	@FXML
-    private ListView listaClientes;
-	
-	@FXML
-    private ComboBox localidad;
-	
-	@FXML
-    private RadioButton monitor1;
-	
-	@FXML
-    private RadioButton monitor2;
-	
-	@FXML
-    private RadioButton monitor3;
-	
-	@FXML
-    private RadioButton monitor4;
-	
-	@FXML
-    private ToggleGroup monitorGroup;
-	
-	@FXML
-    private Label navegacionLabel;
-	
-	@FXML
-    private CheckBox otros1;
-	
-	@FXML
-    private CheckBox otros2;
-	
-	@FXML
-    private CheckBox otros3;
-	
-	@FXML
-    private CheckBox otros4;
+
+	private final Venta ventaBase = new Venta(0, 2, 1, 0, 1, true, true, false, false);
+	private final Vector<String> clientes = new Vector<>();
+	private final Vector<Venta> ventas = new Vector<>();
+	private final ArrayList<Venta> busqueda = new ArrayList<>();
+	private int index;
 
 	@FXML
-    private RadioButton proc1;
-	
+	private TextField nombreField;
+
 	@FXML
-    private RadioButton proc2;
-	
+	private Button botonAniadir;
+
 	@FXML
-    private RadioButton proc3;
-	
+	private Button botonAnterior;
+
 	@FXML
-    private RadioButton proc4;
-	
+	private Button botonBuscar;
+
 	@FXML
-    private ToggleGroup processorGroup;
-	
+	private Button botonCancelar;
+
 	@FXML
-    private RadioButton ram1;
-	
+	private Button botonEliminar;
+
 	@FXML
-    private RadioButton ram2;
-	
+	private Button botonPrimero;
+
 	@FXML
-    private RadioButton ram3;
-	
+	private Button botonSalir;
+
 	@FXML
-    private RadioButton ram4;
-	
+	private Button botonSiguiente;
+
 	@FXML
-    private ToggleGroup ramGroup;
-	
+	private Button botonUltimo;
+
 	@FXML
-    private RadioButton storage1;
-	
+	private ListView listaClientes;
+
 	@FXML
-    private RadioButton storage2;
-	
+	private ComboBox localidad;
+
 	@FXML
-    private RadioButton storage3;
-	
+	private RadioButton monitor1;
+
 	@FXML
-    private RadioButton storage4;
-	
+	private RadioButton monitor2;
+
 	@FXML
-    private ToggleGroup storageGroup;
-	
-	
+	private RadioButton monitor3;
+
+	@FXML
+	private RadioButton monitor4;
+
+	@FXML
+	private ToggleGroup monitorGroup;
+
+	@FXML
+	private Label navegacionLabel;
+
+	@FXML
+	private CheckBox otros1;
+
+	@FXML
+	private CheckBox otros2;
+
+	@FXML
+	private CheckBox otros3;
+
+	@FXML
+	private CheckBox otros4;
+
+	@FXML
+	private RadioButton proc1;
+
+	@FXML
+	private RadioButton proc2;
+
+	@FXML
+	private RadioButton proc3;
+
+	@FXML
+	private RadioButton proc4;
+
+	@FXML
+	private ToggleGroup processorGroup;
+
+	@FXML
+	private RadioButton ram1;
+
+	@FXML
+	private RadioButton ram2;
+
+	@FXML
+	private RadioButton ram3;
+
+	@FXML
+	private RadioButton ram4;
+
+	@FXML
+	private ToggleGroup ramGroup;
+
+	@FXML
+	private RadioButton storage1;
+
+	@FXML
+	private RadioButton storage2;
+
+	@FXML
+	private RadioButton storage3;
+
+	@FXML
+	private RadioButton storage4;
+
+	@FXML
+	private ToggleGroup storageGroup;
+
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-		// TODO
-	}	
-	
+		listaClientes = new ListView<String>();
+		ObservableList<String> items = FXCollections.observableArrayList(
+				"Villalba","Alpedrete","Moralzarzal","Galapagar","Guadarrama");
+		listaClientes.setItems(items);
+	}
+
+	@FXML
+	private void nombreFieldKeyPressed(KeyEvent evt) {
+		if (evt.getCode().equals(KeyCode.ENTER) && valido(nombreField.getText())) { //Si se pulsa ENTER con un nombre valido
+
+		}
+	}
+
+	@FXML
+	private void botonAnteriorActionPerformed(ActionEvent evt) {
+
+	}
+
+	@FXML
+	private void botonCancelarOnActionEvent(ActionEvent evt) {
+
+	}
+
+	@FXML
+	private void botonSalirOnActionEvent(ActionEvent evt) {
+
+	}
+
+	@FXML
+	private void botonAñadirOnActionEvent(ActionEvent evt) {
+
+	}
+
+	@FXML
+	private void botonEliminarOnActionEvent(ActionEvent evt) {
+
+	}
+
+	@FXML
+	private void botonPrimeroOnActionPerformed(ActionEvent evt) {
+
+	}
+
+	@FXML
+	private void botonUltimoOnActionPerformed(ActionEvent evt) {
+
+	}
+
+	@FXML
+	private void botonSiguienteOnActionPerformed(ActionEvent evt) {
+
+	}
+
+	@FXML
+	private void botonBuscarOnActionPerformed(ActionEvent evt) {
+
+	}
+
+	@FXML
+	private void listaClientesOnMouseClicked(ActionEvent evt) {
+
+	}
+
+	private boolean valido(String text) {
+		return text.matches("([a-zA-Z.\\-]+[ ]?)+");
+	}
 }
---
+++ GestorVentanas1FX/src/gestorventanas1fx/Venta.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestorventanas1fx;
+
+/**
+ *
+ * @author alumno
+ */
+public class Venta {
+	private int localizacion;
+	private int procesador;
+	private int ram;
+	private int monitor;
+	private int almacenamiento;
+	private boolean otro1;
+	private boolean otro2;
+	private boolean otro3;
+	private boolean otro4;
+
+	public Venta(int localizacion, int procesador, int ram, int monitor, int almacenamiento, boolean otro1, boolean otro2, boolean otro3, boolean otro4) {
+		this.localizacion = localizacion;
+		this.procesador = procesador;
+		this.ram = ram;
+		this.monitor = monitor;
+		this.almacenamiento = almacenamiento;
+		this.otro1 = otro1;
+		this.otro2 = otro2;
+		this.otro3 = otro3;
+		this.otro4 = otro4;
+	}
+
+	public int getLocalizacion() {
+		return localizacion;
+	}
+
+	public void setLocalizacion(int localizacion) {
+		this.localizacion = localizacion;
+	}
+
+	public int getProcesador() {
+		return procesador;
+	}
+
+	public void setProcesador(int procesador) {
+		this.procesador = procesador;
+	}
+
+	public int getRam() {
+		return ram;
+	}
+
+	public void setRam(int ram) {
+		this.ram = ram;
+	}
+
+	public int getMonitor() {
+		return monitor;
+	}
+
+	public void setMonitor(int monitor) {
+		this.monitor = monitor;
+	}
+
+	public int getAlmacenamiento() {
+		return almacenamiento;
+	}
+
+	public void setAlmacenamiento(int almacenamiento) {
+		this.almacenamiento = almacenamiento;
+	}
+
+	public boolean isOtro1() {
+		return otro1;
+	}
+
+	public void setOtro1(boolean otro1) {
+		this.otro1 = otro1;
+	}
+
+	public boolean isOtro2() {
+		return otro2;
+	}
+
+	public void setOtro2(boolean otro2) {
+		this.otro2 = otro2;
+	}
+
+	public boolean isOtro3() {
+		return otro3;
+	}
+
+	public void setOtro3(boolean otro3) {
+		this.otro3 = otro3;
+	}
+
+	public boolean isOtro4() {
+		return otro4;
+	}
+
+	public void setOtro4(boolean otro4) {
+		this.otro4 = otro4;
+	}
+	
+}
